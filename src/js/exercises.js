37379f93ab09fd985c0186eb2047c8c2f80b9edb
LDR <89.freedom@gmail.com>
2023-02-11 14:23:39 +0600

    finished task on 10/3 and started on 11/1

---
+++ src/js/exercises.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%',
+            java: '60%',
+            pithon: '10%',
+            с: '10%',
+            go: '5%',
+            fortran: '0%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const age = plan.age;
+        const languages = plan.skills.languages;
+        console.log(`Мне ${age} и я владею языками: ${languages}`);
+    }
+};
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+function showProgrammingLangs(plan) {
+
+    const langs = plan.skills.programmingLangs;
+
+    const keyNum = Object.keys(langs);
+    const optNum = Object.values(langs);
+    let answer = [];
+
+    let i = 0;
+    do {
+        answer.push(`Язык ${keyNum[i]} изучен на ${optNum[i]}\n`);
+        i++;
+    } while (i < keyNum.length);
+    if (keyNum.length > 0) {
+        return console.log(answer.join(''));
+    } else {
+        return console.log('');
+    }
+}
+showProgrammingLangs(personalPlanPeter);
