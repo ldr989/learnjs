38343e4d7fca28065c7e10848f10b3c761e01076
LDR <89.freedom@gmail.com>
2023-03-17 17:58:04 +0600

    task 18 completed

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -718,9 +718,9 @@ sortStudentsByGroups(students); */
     }
 } */
 
-console.log(factorial(2.2));
+/* console.log(factorial(2.2)); */
 
-function amountOfPages(summary) {
+/* function amountOfPages(summary) {
 
     const containerOfNumbers = [];
 
@@ -743,4 +743,26 @@ function amountOfPages(summary) {
     return console.log(typeof counter(containerOfNumbers));
 }
 
-amountOfPages(185);
+amountOfPages(185); */
+
+function isPangram(string) {
+    if (
+        string.indexOf('a') >= 1 && string.indexOf('b') >= 1 && string.indexOf('c') >= 1 &&
+        string.indexOf('d') >= 1 && string.indexOf('e') >= 1 && string.indexOf('f') >= 1 &&
+        string.indexOf('g') >= 1 && string.indexOf('h') >= 1 && string.indexOf('i') >= 1 &&
+        string.indexOf('j') >= 1 && string.indexOf('k') >= 1 && string.indexOf('l') >= 1 &&
+        string.indexOf('m') >= 1 && string.indexOf('n') >= 1 && string.indexOf('o') >= 1 &&
+        string.indexOf('p') >= 1 && string.indexOf('q') >= 1 && string.indexOf('r') >= 1 &&
+        string.indexOf('s') >= 1 && string.indexOf('t') >= 1 && string.indexOf('u') >= 1 &&
+        string.indexOf('v') >= 1 && string.indexOf('w') >= 1 && string.indexOf('x') >= 1 &&
+        string.indexOf('y') >= 1 && string.indexOf('z') >= 1 
+        ) {
+        return console.log(true);
+    }
+    else {
+        return console.log(false);
+    }
+}
+
+isPangram('The quick brown fox jumps over the lazy dog');
+
